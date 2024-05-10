fbc3aaba2de3cd678445e6a21fa85d83af4c9c13
visrut <visrut@incubyte.co>
2024-05-10 21:53:25 +0530

    showcase the code to upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,37 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import * as htmlToImage from 'html-to-image';
+import { HighlightAuto, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HighlightAuto],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        themePath: 'assets/styles/solarized-dark.css',
+      },
+    },
+  ],
 })
 export class AppComponent {
   @ViewChild('post') public post!: ElementRef<HTMLDivElement>;
+
+  code = `<html>
+
+<head>
+  <title>My first HTML page</title>
+</head>
+
+<body>
+  <p>Hello World!</p>
+</body>
+
+</html>`;
 
   saveImage() {
     htmlToImage
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHighlightOptions } from 'ngx-highlightjs';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHighlightOptions({
+      fullLibraryLoader: () => import('highlight.js'),
+    }),
+  ],
 };
