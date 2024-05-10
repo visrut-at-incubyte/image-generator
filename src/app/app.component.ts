074a370349effbab215aa9e77d0778a4dd7e7a34
visrut <visrut@incubyte.co>
2024-05-10 20:55:10 +0530

    add image save code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import * as htmlToImage from 'html-to-image';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'image-generator';
+  @ViewChild('post') public post!: ElementRef<HTMLDivElement>;
+
+  saveImage() {
+    htmlToImage
+      .toPng(this.post.nativeElement)
+      .then((dataUrl) => {
+        const link = document.createElement('a');
+        link.download = 'my-image-name.png';
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
